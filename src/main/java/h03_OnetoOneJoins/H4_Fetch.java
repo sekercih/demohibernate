8cd9aa25bf4212ec4c92c1a5fada9ab816466fec
sekercih <hkkskrc@gmail.com>
2021-06-24 23:49:35 +0300

    LAZY FETCH VS EAGER FETCH

--- src/main/java/h03_OnetoOneJoins/H4_Fetch.java
+++ src/main/java/h03_OnetoOneJoins/H4_Fetch.java
@@ -1,42 +1,56 @@
 package h03_OnetoOneJoins;
-
 import java.util.Arrays;
 import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
 public class H4_Fetch {
 	public static void main(String[] args) {
-
-		Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(H1_Ogrenci.class)
+		Configuration con = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(H1_Ogrenci.class)
 				.addAnnotatedClass(H2_Gunluk.class);
 		SessionFactory sf = con.buildSessionFactory();
 		Session session = sf.openSession();
 		Transaction tx = session.beginTransaction();
-		H1_Ogrenci ogr = session.get(H1_Ogrenci.class, 101);
-
+		H1_Ogrenci ogrenci = new H1_Ogrenci();
 		H2_Gunluk gunluk = new H2_Gunluk();
-
-		gunluk = session.get(H2_Gunluk.class, 12);
-
-		System.out.println(gunluk);
-
-		System.out.println(ogr);
-		
-		String sql1="Select o.ogr_id,g.yazilar,o.ogrNot "
-				+ "From ogrenciler o INNER JOIN gunlukler g"
-				+ " ON o.ogr_id=g.ogr_id";
-		List<Object[]> sonucListesi= session.createSQLQuery(sql1).getResultList();		
-		for (Object[] objects : sonucListesi) {
-			System.out.println(Arrays.toString(objects)+"444444");
-		}
-
-		tx.commit();
+		//id=101 olan ogrencinin bilgilerin get() metodu ile sorgulayalim.
+//		ogrenci = session.get(H1_Ogrenci.class, 101);
+//		System.out.println(ogrenci);
+		//id=12 olan gunlugun bilgilerini getirelim.
+//		gunluk = session.get(H2_Gunluk.class, 12);
+//		System.out.println(gunluk);
+		// Ogrenciler ve Gunlukler tablolarindaki ortak olan kayıtların,
+		// Ogrenci adi, gunluk yazisi(yazilar) ve ogrenci notu (ogrNot) bilgilerini sorgulayiniz.
+		// SQL komutlari 
+//		String sql1 = "SELECT o.ogr_adi, g.yazilar, o.ogrNot "
+//					+ "FROM ogrenciler o INNER JOIN gunlukler g "
+//					+ "ON o.ogr_id = g.ogr_id";
+//		
+//		List <Object[]> sonucListesi1 = session.createSQLQuery(sql1).getResultList();
+//		
+//		for(Object [] w: sonucListesi1) {
+//			System.out.println(Arrays.toString(w));
+//		}
+		// HQL komutlari (Sadece Class ve degisken isimleri kullanimalidir. 
+		// Tablo tarafindaki isimlerdirmeler kullanilmaz)
+//		String hql2 = "SELECT o.ogrAd, g.yazilar, o.ogrNot "
+//					+ "FROM H1_Ogrenci o INNER JOIN H2_Gunluk g "
+//					+ "ON o.ogrId = g.ogrenci";
+//	
+//		List<Object[]> sonucListesi2 = session.createQuery(hql2).getResultList();
+//		for(Object [] w: sonucListesi2) {
+//			System.out.println(Arrays.toString(w));
+//		}
+//		
+		// İki tablodaki kayıtlarin herseyini sorgulaylim.
+		String hql3 = "FROM H1_Ogrenci o FULL JOIN H2_Gunluk g "
+				    + "ON o.ogrId = g.ogrenci";
+		List <Object []> sonucListesi3 = session.createQuery(hql3).getResultList();
+		sonucListesi3.stream().forEach((x) -> System.out.println(Arrays.toString(x)));
+		tx.commit()	;
 		session.close();
 		sf.close();
 	}
-
 }
---
+++ src/main/java/h04_OneToManyJoins/H1_Ogrenci.java
@@ -0,0 +1,64 @@
+package h04_OneToManyJoins;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+@Entity
+@Table(name="ogrenciler1")
+ public class H1_Ogrenci {
+	@Id
+	private int ogrId;
+	private String ogrAd;
+	private int ogrNot;
+	@OneToMany(mappedBy="ogrenci",orphanRemoval = true,cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+	private List <H2_Kitap> kitapListesi = new ArrayList<>();
+	public H1_Ogrenci() {
+	}
+	public H1_Ogrenci(int ogrId, String ogrAd, int ogrNot) {
+		this.ogrId = ogrId;
+		this.ogrAd = ogrAd;
+		this.ogrNot = ogrNot;
+	}
+	public int getOgrId() {
+		return ogrId;
+	}
+	public void setOgrId(int ogrId) {
+		this.ogrId = ogrId;
+	}
+	public String getOgrAd() {
+		return ogrAd;
+	}
+	public void setOgrAd(String ogrAd) {
+		this.ogrAd = ogrAd;
+	}
+	public int getOgrNot() {
+		return ogrNot;
+	}
+	public void setOgrNot(int ogrNot) {
+		this.ogrNot = ogrNot;
+	}
+	public List<H2_Kitap> getKitapListesi() {
+		return kitapListesi;
+	}
+	public void setKitapListesi(List<H2_Kitap> kitapListesi) {
+		this.kitapListesi = kitapListesi;
+	}
+	@Override
+	public String toString() {
+		return "Ogrenci ogrId=" + ogrId + ", ogrAd=" + ogrAd + ", ogrNot=" + ogrNot ;
+		
+	}
+//	@Override
+//	public String toString() {
+//		return "Ogrenci ogrId=" + ogrId + ", ogrAd=" + ogrAd + ", ogrNot=" + ogrNot + ", kitapListesi="
+//				+ kitapListesi + "]";
+//		
+//	}
+}
